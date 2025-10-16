57d8001dee098f3536c4e5beaaa7034611dff8c4
milaress <milaresspotter@gmail.com>
2025-10-16 16:06:57 +0200

    Initial commit

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import '../App.css'
+import { FaUser, FaListAlt, FaBuilding, FaHistory } from "react-icons/fa";
 function Sidebar() {
   return (
     <div className="sidebar">
         <div className="logo"><a>WedHall</a></div>
         <div className="menu">
             <ul>
-                <li><a href="#"><i></i>My Profile</a></li>
-                <li><a href="#"><i></i>Requests</a></li>
-                <li><a href="#"><i></i>My Halls</a></li>
-                <li><a href="#"><i></i>History</a></li>
+              <li><a href="#"><FaUser /> My Profile</a></li>
+              <li><a href="#"><FaListAlt /> Requests</a></li>
+              <li><a href="#"><FaBuilding /> My Halls</a></li>
+              <li><a href="#"><FaHistory /> History</a></li>
             </ul>
         </div>
         <div className="log-out">Log out</div>
